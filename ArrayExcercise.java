4c6175a503330db56ac837b0bf9f71e4d0306add
Daniel Fabian <36898378+Dfab27@users.noreply.github.com>
2026-01-06 13:18:14 -0500

    Refactor ArrayExcercise for duplicate and element removal
    Updated the array exercise to remove duplicates and elements more efficiently. Added a new method for removing specific elements and modified the main method to demonstrate functionality.

--- ArrayExcercise.java
+++ ArrayExcercise.java
@@ -1,42 +1,33 @@
 /**
- * Given an integer array nums sorted in non-decreasing order, remove the duplicates 
- * in-place such that each unique element appears only once. The relative order of the elements 
- * should be kept the same. Consider the number of unique elements in nums to be k​​​​​​​​​​​​​​. After removing 
- * duplicates, return the number of unique elements k. The first k elements of nums should contain
- *  the unique numbers in sorted order. The remaining elements beyond index k - 1 can be ignored.
+ * These exercise come from problem you see in Neetcode.io
  */
 public class ArrayExcercise {
 
 	public static void main(String[] args) {
-		int nums[] = {0, 0, 1, 1, 1, 2, 2, 3, 3, 4};
+		int nums[] = { 1, 1, 2, 3, 4 };
 
 		toString(nums);
+		
 
-		int n = removeDuplicates(nums);
-		System.out.println(n);
-
+		System.out.println(removeElement(nums, 1));
+		
 		toString(nums);
 
 	}
 
 	public static int removeDuplicates(int[] nums) {
 		// sets the number of unique value.
-		int k = 0;
+		if (nums.length == 0)
+			return 0;
+
+		// elements starts at one.
+		int k = 1;
 
 		// loop that traverses the array
-		for (int i = 0; i < nums.length; i++) {
-			if(nums[i] != -1) k++;
-			for (int n = i + 1; n < nums.length; n++) {
-				if ((nums[i] == nums[n])) {
-					nums[n] = -1;
-
-				} else if (nums[i] == -1) {
-					nums[i] = nums[n];
-					nums[n] = -1;
-				} else {
-					k++;
-					break;
-				}
+		for (int i = 1; i < nums.length; i++) {
+			if (nums[i] != nums[i - 1]) {
+				nums[k] = nums[i];
+				k++;
 			}
 
 		}
@@ -51,6 +42,31 @@ public class ArrayExcercise {
 		}
 
 		System.out.println();
+	}
+
+	/**
+	 * 
+	 * @param nums is an array that contains int values
+	 * @param val the value to be removed
+	 * @return the number of time val does not appear
+	 */
+	public static int removeElement(int[] nums, int val) {
+		
+		if(nums.length == 0) return 0;
+		
+		//counts
+		// how many different 
+		int k = 0;
+		
+		for(int i = 0; i < nums.length; i++) {
+			if(nums[i] != val) {
+				nums[k] = nums[i];
+				k++;
+			}
+		} 
+		
+		return k;
+
 	}
 
 }
